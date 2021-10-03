b99a48feadf1922ea00bcc16be2b55dc41dd06eb
DavideBernardini <davidebernardini09@gmail.com>
2021-10-03 17:30:49 +0200

    Aggiunto Header

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="d-flex align-items-center ps-3">
+        <img :src="require(`../assets/images/${logoImage}`)" alt="logo spotify green">
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: ['logoImage']
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/style/variables.scss";
+
+header {
+    height: 70px;
+    background-color: $secondaryColor;
+    img {
+        height: 45px;
+    }
+}
+</style>
